5368b11ea5cf6ecd89ab7d3258a50586e4989293
YemaneZewdu <yemane18berhan@gmail.com>
2020-07-22 22:47:49 -0400

    Added injecting values from properties file using @Value annotation

--- spring-demo-annotations/src/com/mycompany/springannotationdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/mycompany/springannotationdemo/AnnotationDemoApp.java
@@ -15,11 +15,15 @@ public class AnnotationDemoApp {
 		//Coach theCoach = context.getBean("theTennisCoach",Coach.class);
 		Coach theCoach = context.getBean("tennisCoach",Coach.class);
 	
+		// used with value injection from properties file 
+		//SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		// call method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
 		
+
+
 		//close the context
 		context.close();
 	}
